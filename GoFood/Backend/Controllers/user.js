55ec684e78f705a7d60bd323f675a63ee35cc96c
Chaitanya Zunzurkar <chaitanyazunzurkar09@gmail.com>
2024-12-14 20:43:44 +0530

    Adding hashing functionality and camparing that hashedpassword while signup and signin

--- GoFood/Backend/Controllers/user.js
+++ GoFood/Backend/Controllers/user.js
@@ -1,5 +1,7 @@
 const User = require('../Models/Users')
 const { body , validationResult } = require('express-validator')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 const validateSignup = [
     body('name').notEmpty().withMessage("Name should not be empty"),
@@ -12,6 +14,9 @@ const validateSignin = [
     body('email').isEmail().withMessage("Invalid email"),
     body('password').isLength({ min : 6 }).withMessage("Password must be at least 6 characters long")
 ];
+
+require('dotenv').config()
+const SECRET_KEY = process.env.SECRET_KEY
 
 async function SignupHandler(req , res) {
     try {
@@ -32,20 +37,29 @@ async function SignupHandler(req , res) {
             })
         }
 
+        let hashedPassword
+        try {
+            let salt = await bcrypt.genSalt(10)
+            hashedPassword = await bcrypt.hash(password , salt)
+        }
+        catch(error) {
+            res.status(500).json({
+                success:false,
+                message:"Fail to hash password"
+            })
+        }
+
         const user = await User.create({
             name,
             email,
-            password,
+            password: hashedPassword,
             location,
         })
 
         res.status(201).json({
             success:true,
             message:"Signup Successfully",
-            User : {
-                name:user.name,
-                email:user.email,
-            }
+            User : user
         })
     }
     catch(error) {
@@ -61,31 +75,55 @@ async function SigninHandler(req ,res) {
     try {
         const errors = validationResult(req)
         if(!errors.isEmpty()) {
-            res.status(400).json({
+            return res.status(400).json({
                 success:false,
                 errors: errors.array()
             })
         }
+
         const { email , password } = req.body;
         const user = await User.findOne({ email })
 
         if(!user) {
             return res.status(400).json({
                 success:false,
-                message:"Invalid email or password."
+                message:"User does not exist"
             })
         }
 
-        if(user.password !== password) {
+        // creating payload 
+        const payload = {
+            id: user.id,
+            email: user.email
+        }
+
+        const compredPassword = await bcrypt.compare(password , user.password)
+        if(!compredPassword) {
             return res.status(400).json({
                 success:false,
                 message:"Invalid email or password."
             })
         }
 
-        res.status(200).json({
+
+        const token = await jwt.sign(payload , SECRET_KEY , {
+            expiresIn: "24h"
+        })
+
+        const options = {
+            expires: new Date(Date.now() + 1 * 24 * 60 * 60),
+            httpOnly: true
+        }
+
+        res.cookie("token" , token , options).status(200).json({
             success:true,
-            message:"Signin successful"
+            message:"Signin successful",
+            User: {
+                name:user.name,
+                email:user.email,
+                token,
+                message: "User login successfully."
+            }
         })
     }
     catch(error) {
